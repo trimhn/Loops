a8b3b3a6c0aa752d9acdf2d45650b3973a336179
(trimhn) <trimhn@gmail.com>
2019-01-17 15:39:35 -0500

    FizzBuzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,23 @@
+import java.sql.SQLOutput;
+
+public class FizzBuzz {
+    public static void main(String[]args){
+        System.out.println("FizzBuzz game");
+
+
+    {for (int i=1; i<=100;i++){
+
+         if ((i%3==0)&&(i%5==0))
+            System.out.println("FizzBuzz");
+         else if (i%3==0)
+            System.out.println("Fizz");
+         else if (i%5==0)
+             System.out.println("Buzz");
+        else
+
+        System.out.println(i);
+    }
+
+
+    }
+}}
