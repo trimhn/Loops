02862cf8c6fa146764afa5c3f16119f3a8391949
(trimhn) <trimhn@gmail.com>
2019-01-17 16:37:32 -0500

    with all assingment from 1-11

---
+++ src/Star2.java
@@ -0,0 +1,16 @@
+public class Star2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 4; j >= i; j--) {
+                System.out.print(" ");
+
+            }
+            for(int k=1; k<=i;k++)
+                {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
--- src/Stars1.java
+++ src/Stars1.java
@@ -1,7 +1,8 @@
 public class Stars1 {
     public static void main (String[]args){
-        for(int i=0;i<5;i++) {
-            for (int j = 4; j > i; j--) {
-                System.out.println( i );
+        for(int i=1;i<=5;i++) {
+            for (int j = 1; j<=i; j++) {
+                System.out.print( "*" );
             }
+            System.out.println();
         }}}
